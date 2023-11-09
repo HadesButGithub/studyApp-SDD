f196c54857775b6fb1351f772dfa314599330111
Harry Lewandowski <harrylewandowski04@gmail.com>
2023-11-09 13:46:09 +1100

    Initial Commit

---
+++ studyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  studyApp
+//
+//  Created by Harry Lewandowski on 9/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ studyApp/studyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  studyAppApp.swift
+//  studyApp
+//
+//  Created by Harry Lewandowski on 9/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct studyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
