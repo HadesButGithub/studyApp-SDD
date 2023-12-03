a73bb795db5a23912323468383a235cb6b18a581
Harry Lewandowski <harrylewandowski04@gmail.com>
2023-12-04 10:39:39 +1100

    Built basic user interface

--- studyApp/ContentView.swift
+++ studyApp/ContentView.swift
@@ -16,7 +16,76 @@ struct ContentView: View {
                     .fontWeight(.heavy)
             }
             Spacer()
+            Button(action: {
+                // add action
+            }) {
+                HStack {
+                    Image(systemName: "play.fill")
+                        .foregroundColor(Color.white)
+                    
+                    Text("Start")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                }
+                .frame(width: 300.0, height: 95.0)
+                .background(.blue)
+                .cornerRadius(20.0)
+            }
             
+            Button(action: {
+                // add action
+            }) {
+                HStack {
+                    Image(systemName: "book.fill")
+                        .foregroundColor(Color.white)
+                    
+                    Text("Study Types")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                }
+                .frame(width: 250, height: 75)
+                .background(.blue)
+                .cornerRadius(20.0)
+            }
+            
+            Button(action: {
+                // add action
+            }) {
+                HStack {
+                    Image(systemName: "bell.fill")
+                        .foregroundColor(Color.white)
+                    
+                    Text("Reminders")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                }
+                .frame(width: 250, height: 75)
+                .background(.blue)
+                .cornerRadius(20.0)
+            }
+            
+            Button(action: {
+                // add action
+            }) {
+                HStack {
+                    Image(systemName: "bubble.fill")
+                        .foregroundColor(Color.white)
+                    
+                    Text("Language")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                }
+                .frame(width: 250, height: 75)
+                .background(.blue)
+                .cornerRadius(20.0)
+            }
+            
+
+            Spacer()
             
             
         }
