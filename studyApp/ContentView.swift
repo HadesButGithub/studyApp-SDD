1e36a17fe4192d7838115528f72f903607de362e
Harry Lewandowski <harrylewandowski04@gmail.com>
2023-12-05 12:04:35 +1100

    Made the buttons actually work

--- studyApp/ContentView.swift
+++ studyApp/ContentView.swift
@@ -9,90 +9,103 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
+        
+        NavigationView {
+            VStack {
                 Text("studyApp")
                     .font(.title)
                     .fontWeight(.heavy)
-            }
-            Spacer()
-            Button(action: {
-                // add action
-            }) {
-                HStack {
-                    Image(systemName: "play.fill")
-                        .foregroundColor(Color.white)
-                    
-                    Text("Start")
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundColor(Color.white)
+                    .padding(.bottom, 20)
+                
+                Spacer()
+                
+                NavigationLink(destination: studyView()) {
+                    mainButton(imageName: "play.fill", text: "Start")
                 }
-                .frame(width: 300.0, height: 95.0)
-                .background(.blue)
-                .cornerRadius(20.0)
-            }
-            
-            Button(action: {
-                // add action
-            }) {
-                HStack {
-                    Image(systemName: "book.fill")
-                        .foregroundColor(Color.white)
-                    
-                    Text("Study Types")
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                        .foregroundColor(Color.white)
+                .padding(.bottom, 20)
+                
+                NavigationLink(destination: settingsPage()) {
+                    secondaryButton(imageName: "book.fill", text: "Study Types")
                 }
-                .frame(width: 250, height: 75)
-                .background(.blue)
-                .cornerRadius(20.0)
-            }
-            
-            Button(action: {
-                // add action
-            }) {
-                HStack {
-                    Image(systemName: "bell.fill")
-                        .foregroundColor(Color.white)
-                    
-                    Text("Reminders")
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                        .foregroundColor(Color.white)
+                
+                NavigationLink(destination: remindersView()) {
+                    secondaryButton(imageName: "bell.fill", text: "Reminders")
                 }
-                .frame(width: 250, height: 75)
-                .background(.blue)
-                .cornerRadius(20.0)
-            }
-            
-            Button(action: {
-                // add action
-            }) {
-                HStack {
-                    Image(systemName: "bubble.fill")
-                        .foregroundColor(Color.white)
-                    
-                    Text("Language")
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                        .foregroundColor(Color.white)
+                
+                Menu {
+                    Text("English")
+                    Text("French")
+                    Text("Spanish")
+                } label: {
+                    secondaryButton(imageName: "bubble.fill", text: "Language")
                 }
-                .frame(width: 250, height: 75)
-                .background(.blue)
-                .cornerRadius(20.0)
+                Spacer()
             }
-            
-
-            Spacer()
-            
-            
+            .padding()
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct mainButton: View {
+    var imageName: String
+    var text: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: imageName)
+                .foregroundColor(.white)
+            
+            Text(text)
+                .font(.title3)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+        }
+        .frame(width: 300, height: 100)
+        .background(Color.blue)
+        .cornerRadius(20.0)
+    }
+}
+
+struct secondaryButton: View {
+    var imageName: String
+    var text: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: imageName)
+                .foregroundColor(.white)
+            
+            Text(text)
+                .font(.title3)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+        }
+        .frame(width: 250, height: 75)
+        .background(Color.blue)
+        .cornerRadius(20.0)
+    }
+}
+
+struct studyView: View {
+    var body: some View {
+        Text("hello world! this is the study view")
+    }
+}
+
+struct settingsPage: View {
+    var body: some View {
+        Text("hello world! this is the settings page")
+    }
+}
+
+struct remindersView: View {
+    var body: some View {
+        Text("hello world! this is the reminders view")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
